48c9fb7c37675870b92142f69ba61017df467398
medinalicina <medinalicina94@gmail.com>
2023-09-15 18:31:36 +0200

    nis

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/card';
+// import logo from './logo.svg';
+// import './App.css';
+// import Card from './components/card';
 
-function App() {
-  return ( 
-  <div>
+// function App() {
+//   return ( 
+//   <div>
 
-  <Card />
-  </div>
+//   <Card />
+//   </div>
     
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import Card from './Components/Card.js';
+
+const App = () => {
+const odeljenje =[{ime: "medina" , prezime:"licina", godine:"16"}, {ime:"dzemila", prezime:"mulic", godine:"16"},{}, {}, {}];
+
+{odeljenje.map((el) =>
+  <Card></Card>)}
+
+
+
+
+
+  return (
+    <div>
+      <Card ime ={"Iman"} datum={"14.12"}/>
+      <Card ime= {"najda"} datum={"8.6"}/>
+      <Card ime ={"sara"} datum={"14.12"}/>
+      <Card ime= {"ema"} datum={"8.6"}/>
+      <Card ime ={"hana"} datum={"14.12"}/>
+      <Card ime= {"emina"} datum={"8.6"}/>
+      
+    </div>
   );
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,37 @@
 import React from "react"
+import "./Card.css"
 
-
-
-
-function Card() {
-return <div style={width}>
-<h1>Medina Licina</h1>
-<p>0645280490</p>
-<p>vase imee???</p>
-</div>;
+function Card(props){
+    
+return( 
+    <div className="card">
+        <h1>{props.ime}</h1>
+        <h1>{props.prezime}</h1>
+        <h1>{props.godine}</h1>
+</div>
+)
 }
+
+
+
+
+
+
+
+
+// function Card() {
+// return <div style={{
+//     width: "350",
+//     minHeight: ""
+// }}>
+// <h1>Medina Licina</h1>
+// <h1>12.15.2007.</h1>
+// <h1>0645280490</h1>
+// <h1>Veljka Vlahovica </h1>
+// <h2>medicinska skola</h2>
+// </div>;
+// }
+//niz objekata da mapiramo i da pokazemo 
+
 
 export default Card;
